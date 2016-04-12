42bf06df43dcb79d936c2eddbe269976ad09bbdc
Jason Zheng <quietjason@gmail.com>
2016-04-13 07:30:47 +0800

    Init 32_Storyboards/MirrorChatter

---
+++ 32_Storyboards/MirrorChatter/MirrorChatter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MirrorChatter
+//
+//  Created by Jason Zheng on 4/13/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
