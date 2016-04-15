5e176dc313f4b2fb831158d277f2c9baaae737f4
Jason Zheng <quietjason@gmail.com>
2016-04-15 08:12:46 +0800

    Init 26_LocalizationAndBundles/LocalizationDemo.

---
+++ 26_LocalizationAndBundles/LocalizationDemo/LocalizationDemo/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LocalizationDemo
+//
+//  Created by Jason Zheng on 4/15/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+}
+
---
+++ 26_LocalizationAndBundles/LocalizationDemo/LocalizationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by Jason Zheng on 4/15/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+}
+
