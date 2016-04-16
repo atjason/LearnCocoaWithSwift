a3ecbc8c7157fd1a282f004871de0d176e8353bb
Jason Zheng <quietjason@gmail.com>
2016-04-17 07:59:11 +0800

    Add 35_NSTask/FileList.

---
+++ 35_NSTask/FileList/FileList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileList
+//
+//  Created by Jason Zheng on 4/17/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
