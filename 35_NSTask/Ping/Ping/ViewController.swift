6ba95c77a541078d71ed8708a19ca21ba75290a7
Jason Zheng <quietjason@gmail.com>
2016-04-17 10:27:58 +0800

    Implement task.

--- 35_NSTask/Ping/Ping/ViewController.swift
+++ 35_NSTask/Ping/Ping/ViewController.swift
@@ -10,10 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
   
+  dynamic var domain = ""
+  
+  @IBOutlet weak var pingButton: NSButton!
   @IBOutlet var textView: NSTextView!
+  
+  let notificationCenter = NSNotificationCenter.defaultCenter()
+  
+  var task: NSTask?
+  var pipe: NSPipe?
+  var fileHandle: NSFileHandle?
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  deinit {
+    notificationCenter.removeObserver(self)
   }
 
   // MARK: - Action
@@ -21,14 +34,66 @@ class ViewController: NSViewController {
   @IBAction func ping(sender: NSButton) {
     
     if sender.state == NSOnState {
-      if let textStorage = textView.textStorage {
-        textStorage.replaceCharactersInRange(NSRange(0..<textStorage.length), withString: "Ping...")
-      }
+      
+      task = NSTask()
+      task?.launchPath = "/sbin/ping"
+      task?.arguments = ["-c4", domain]
+      
+      pipe = NSPipe()
+      task?.standardOutput = pipe
+      
+      fileHandle = pipe?.fileHandleForReading
+      
+      notificationCenter.removeObserver(self)
+      notificationCenter.addObserver(self,
+        selector: #selector(ViewController.receiveDataReadyNotification(_:)),
+        name: NSFileHandleReadCompletionNotification, object: fileHandle)
+      notificationCenter.addObserver(self,
+        selector: #selector(ViewController.receiveTerminateNotification(_:)),
+        name: NSTaskDidTerminateNotification, object: task)
+      
+      task?.launch()
+      
+      clearString()
+      fileHandle?.readInBackgroundAndNotify()
       
     } else {
-      if let textStorage = textView.textStorage {
-        textStorage.replaceCharactersInRange(NSRange(0..<textStorage.length), withString: "Ping stopped.")
+      task?.interrupt()
+    }
+  }
+  
+  // MARK: - Notification
+  
+  func receiveDataReadyNotification(notification: NSNotification) {
+    if let data = notification.userInfo?[NSFileHandleNotificationDataItem] as? NSData {
+      if let string = NSString(data: data, encoding: NSUTF8StringEncoding) as? String {
+        appendString(string)
       }
+    }
+    
+    // Continue to read if fileHandle isn't nil.
+    fileHandle?.readInBackgroundAndNotify()
+  }
+  
+  func receiveTerminateNotification(notification: NSNotification) {
+    task = nil
+    pipe = nil
+    fileHandle = nil
+    
+    pingButton.state = NSOffState
+  }
+  
+  // MARK: - Helper
+  
+  func clearString() {
+    if let textStorage = textView.textStorage {
+      textStorage.replaceCharactersInRange(NSRange(0..<textStorage.length), withString: "")
+    }
+  }
+  
+  func appendString(string: String) {
+    if let textStorage = textView.textStorage {
+      textStorage.replaceCharactersInRange(NSRange(location: textStorage.length, length: 0), withString: string)
     }
   }
 }
