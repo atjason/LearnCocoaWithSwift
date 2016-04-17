9de771b380dcd04af29204e566dcef1ece47b08e
Jason Zheng <quietjason@gmail.com>
2016-04-17 08:09:47 +0800

    Init project.

--- 35_NSTask/FileList/FileList/ViewController.swift
+++ 35_NSTask/FileList/FileList/ViewController.swift
@@ -10,10 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+  @IBOutlet var textView: NSTextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
   }
 
   override var representedObject: AnyObject? {
@@ -22,6 +23,46 @@ class ViewController: NSViewController {
     }
   }
 
+  // MARK: - Action
+  
+  @IBAction func openFolder(sender: NSButton) {
+    let defaultFolderURL: NSURL
+    do {
+      defaultFolderURL = try NSFileManager.defaultManager().URLForDirectory(
+        NSSearchPathDirectory.DownloadsDirectory,
+        inDomain: NSSearchPathDomainMask.UserDomainMask,
+        appropriateForURL: nil, create: false)
+      
+    } catch {
+      defaultFolderURL = NSURL(fileURLWithPath: "~/Downloads")
+    }
+    
+    let openPanel = NSOpenPanel()
+    openPanel.directoryURL = defaultFolderURL
+    openPanel.canChooseDirectories = true
+    openPanel.canChooseFiles = false
+    openPanel.prompt = "Open"
+    
+    let selection = openPanel.runModal()
+    
+    if selection == NSFileHandlingPanelOKButton {
+      if let selectedURL = openPanel.URL {
+        if let content = getFileListOf(selectedURL) {
+          if let attrStr = textView.textStorage as? NSMutableAttributedString {
+            attrStr.replaceCharactersInRange(NSRange(0..<attrStr.length), withString: content)
+          }
+        }
+      }
+    }
 
+  }
+  
+  // MARK: - Helper
+  
+  func getFileListOf(path: NSURL) -> String? {
+    
+//    return nil
+    return path.absoluteString
+  }
 }
 
