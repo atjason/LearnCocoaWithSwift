4a3d81f6bd18471f1953ba9f4175a6ba74c48185
Jason Zheng <quietjason@gmail.com>
2016-04-19 06:33:13 +0800

    Add layout for MainWindowController and bind it.

--- 24_Sheet/SheetDemo/SheetDemo/MainWindowController.swift
+++ 24_Sheet/SheetDemo/SheetDemo/MainWindowController.swift
@@ -9,7 +9,36 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+  
+  @IBOutlet var textView: NSTextView!
+  
+  var addPersonWindowController: AddPersonWindowController?
+  
   override var windowNibName: String? {
     return "MainWindowController"
+  }
+  
+  // MARK: - Action
+  
+  @IBAction func addPerson(sender: NSButton) {
+    if addPersonWindowController == nil {
+      addPersonWindowController = AddPersonWindowController()
+    }
+    
+    if let window = addPersonWindowController?.window {
+      self.window?.beginSheet(window, completionHandler: addNewPerson)
+    }
+  }
+  
+  // MARK: - Helper
+  
+  func addNewPerson(response: NSModalResponse) {
+    if response == NSModalResponseOK {
+      if let description = addPersonWindowController?.person.description {
+        if textView.string != nil {
+          textView.string? += description + "\n"
+        }
+      }
+    }
   }
 }
