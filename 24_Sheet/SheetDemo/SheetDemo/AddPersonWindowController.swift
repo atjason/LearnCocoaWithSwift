fdb3b2a5050b496c1ac6db0ad7352c3c7feedfc4
Jason Zheng <quietjason@gmail.com>
2016-04-19 06:05:52 +0800

    Add class class AddPersonWindowController.

---
+++ 24_Sheet/SheetDemo/SheetDemo/AddPersonWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  AddPersonWindowController.swift
+//  SheetDemo
+//
+//  Created by Jason Zheng on 4/19/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class AddPersonWindowController: NSWindowController {
+
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+  }
+  
+}
