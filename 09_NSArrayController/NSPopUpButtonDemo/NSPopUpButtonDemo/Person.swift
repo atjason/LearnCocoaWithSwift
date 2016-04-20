61cb7f64ef588f4af518c0488bea6c3b744eaf64
Jason Zheng <quietjason@gmail.com>
2016-04-20 17:27:01 +0800

    Add class Person.

---
+++ 09_NSArrayController/NSPopUpButtonDemo/NSPopUpButtonDemo/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  NSPopUpButtonDemo
+//
+//  Created by Jason Zheng on 4/20/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+  var name = ""
+  var age = 0
+  
+  init(name: String, age: Int) {
+    self.name = name
+    self.age = age
+  }
+}
