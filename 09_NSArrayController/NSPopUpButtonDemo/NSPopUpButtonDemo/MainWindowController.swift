b0343c189d432ae4f7f48dbdbed3fd985c775f71
Jason Zheng <quietjason@gmail.com>
2016-04-20 17:38:18 +0800

    Bind pop up button with personArray.

--- 09_NSArrayController/NSPopUpButtonDemo/NSPopUpButtonDemo/MainWindowController.swift
+++ 09_NSArrayController/NSPopUpButtonDemo/NSPopUpButtonDemo/MainWindowController.swift
@@ -9,7 +9,17 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+  
+  dynamic var personArray = [Person]()
+  
   override var windowNibName: String? {
     return "MainWindowController"
+  }
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    
+    personArray.append(Person(name: "Tom", age: 30))
+    personArray.append(Person(name: "Jack", age: 29))
   }
 }
