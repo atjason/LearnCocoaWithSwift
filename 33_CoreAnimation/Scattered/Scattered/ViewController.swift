705a320824c6fa90f5f4ba6210771aed87f162bb
Jason Zheng <quietjason@gmail.com>
2016-04-21 08:29:47 +0800

    Add 33_CoreAnimation/Scattered.

---
+++ 33_CoreAnimation/Scattered/Scattered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scattered
+//
+//  Created by Jason Zheng on 4/21/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
