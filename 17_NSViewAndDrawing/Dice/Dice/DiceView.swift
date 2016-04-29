f16fd467b416c6f80f3edd0f1319809c7c9ea68c
Jason Zheng <quietjason@gmail.com>
2016-04-30 07:59:34 +0800

    Add func metricsForSize.

--- 17_NSViewAndDrawing/Dice/Dice/DiceView.swift
+++ 17_NSViewAndDrawing/Dice/Dice/DiceView.swift
@@ -9,6 +9,7 @@
 import Cocoa
 
 class DiceView: NSView {
+  
   override func drawRect(dirtyRect: NSRect) {
     NSColor.lightGrayColor().set()
     NSBezierPath.fillRect(bounds)
@@ -18,5 +19,15 @@ class DiceView: NSView {
     path.moveToPoint(NSPoint(x: 0, y: 0))
     path.lineToPoint(NSPoint(x: bounds.width, y: bounds.height))
     path.stroke()
+  }
+  
+  func metricsForSize(size: NSSize) -> (edgeLength: CGFloat, dieFrame: NSRect) {
+    let edgeLength = min(size.width, size.height)
+    
+    let padding = edgeLength / 10.0
+    let drawingBounds = NSRect(x: 0, y: 0, width: edgeLength, height: edgeLength)
+    let dieFrame = drawingBounds.insetBy(dx: padding, dy: padding)
+    
+    return (edgeLength, dieFrame)
   }
 }
