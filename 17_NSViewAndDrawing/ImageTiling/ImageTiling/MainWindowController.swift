03b021fd3f085e74f90d48c1413000902aa9c395
Jason Zheng <quietjason@gmail.com>
2016-05-01 08:18:31 +0800

    Init the window.

---
+++ 17_NSViewAndDrawing/ImageTiling/ImageTiling/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  ImageTiling
+//
+//  Created by Jason Zheng on 5/1/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
