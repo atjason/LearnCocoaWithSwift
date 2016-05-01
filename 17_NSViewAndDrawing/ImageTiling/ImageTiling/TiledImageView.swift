3dddc2abd25bf8c433b74387b471e7433af20261
Jason Zheng <quietjason@gmail.com>
2016-05-01 08:23:53 +0800

    Show image in class TiledImageView.

--- 17_NSViewAndDrawing/ImageTiling/ImageTiling/TiledImageView.swift
+++ 17_NSViewAndDrawing/ImageTiling/ImageTiling/TiledImageView.swift
@@ -8,7 +8,31 @@
 
 import Cocoa
 
-class TiledImageView: NSView {
+@IBDesignable class TiledImageView: NSView {
+  @IBInspectable var image: NSImage?
+  let rowCount = 5
+  let columnCount = 5
   
+  override func drawRect(dirtyRect: NSRect) {
+    if let image = image {
+      for x in (0..<rowCount) {
+        for y in (0..<columnCount) {
+          let frame = frameForImageAtLogicalX(x, y: y)
+          image.drawInRect(frame)
+        }
+      }
+    }
+  }
+  
+  // MARK: - Drawing
+  
+  func frameForImageAtLogicalX(logicalX: Int, y logicalY: Int) -> NSRect {
+    let spacing = 10
+    let width = 100
+    let height = 100
+    let x = (spacing + width) * logicalX
+    let y = (spacing + height) * logicalY
+    return NSRect(x: x, y: y, width: width, height: height)
+  }
 }
 
