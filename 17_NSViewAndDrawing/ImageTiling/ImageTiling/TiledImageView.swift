178439107034a02810645150568796a7a59dd9c2
Jason Zheng <quietjason@gmail.com>
2016-05-01 08:19:18 +0800

    Add class TiledImageView.

---
+++ 17_NSViewAndDrawing/ImageTiling/ImageTiling/TiledImageView.swift
@@ -0,0 +1,14 @@
+//
+//  TiledImageView.swift
+//  ImageTiling
+//
+//  Created by Jason Zheng on 5/1/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class TiledImageView: NSView {
+  
+}
+
