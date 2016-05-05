4968fa5ccf32ed64ef7992b9d779de94e50400ec
Jason Zheng <quietjason@gmail.com>
2016-05-05 11:37:26 +0800

    Add 20_AttributedString/Dice.

---
+++ 20_AttributedString/Dice/Dice/MainWindowController.swift
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.swift
+//  Dice
+//
+//  Created by Jason Zheng on 4/30/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
