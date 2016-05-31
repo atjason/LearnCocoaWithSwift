d3408aba6f2aa43c210be9295cec8ba6ed603d0e
Jason <quietjason@gmail.com>
2016-06-01 07:44:07 +0800

    Add completionHandler.

---
+++ 28_WebServices/RanchForecast/RanchForecast/ScheduleFetcher.swift
@@ -0,0 +1,42 @@
+//
+//  ScheduleFetcher.swift
+//  RanchForecast
+//
+//  Created by Jason Zheng on 6/1/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Foundation
+
+class ScheduleFetcher {
+  enum FetchCoursesResult {
+    case Succeed([Course])
+    case Failed(NSError)
+  }
+  
+  let session: NSURLSession
+  
+  init() {
+    let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+    session = NSURLSession(configuration: config)
+  }
+  
+  func fetchCourses(completionHandler: (FetchCoursesResult) -> Void) {
+    let url = NSURL(string: "http://bookapi.bignerdranch.com/courses.json")!
+    let request = NSURLRequest(URL: url)
+    let task = session.dataTaskWithRequest(request) { (data, response, error) in
+      var fetchCourseResult: FetchCoursesResult
+      if let data = data {
+        print("Data length: \(data.length)")
+        fetchCourseResult = FetchCoursesResult.Succeed([])
+      } else {
+        fetchCourseResult = FetchCoursesResult.Failed(error!)
+      }
+      
+      NSOperationQueue.mainQueue().addOperationWithBlock {
+        completionHandler(fetchCourseResult)
+      }
+    }
+    task.resume()
+  }
+}
