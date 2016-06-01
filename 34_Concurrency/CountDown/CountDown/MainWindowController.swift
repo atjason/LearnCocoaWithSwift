9e04ec68146a8fd1793b84085ced20455b18809c
Jason <quietjason@gmail.com>
2016-06-02 07:40:44 +0800

    Add 34_Concurrency/CountDown.

---
+++ 34_Concurrency/CountDown/CountDown/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  CountDown
+//
+//  Created by Jason Zheng on 6/2/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
